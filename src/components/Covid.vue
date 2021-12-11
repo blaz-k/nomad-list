914b0833fc15daa88ccdbb4a82e8fc25ae8fcd43
Blaz <blazyy@gmail.com>
2021-12-11 14:27:35 +0100

    covid component created

---
+++ src/components/Covid.vue
@@ -0,0 +1,30 @@
+<template>Covid request component</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Covid",
+  props: ["showCountryCapital"],
+  created() {
+    this.getCovid();
+  },
+
+  data() {
+    return {
+      //Covid data:
+      covidUrl: "https://covid2019-api.herokuapp.com/country/",
+      covidResults: null,
+      countryCovid: this.showCountryCapital.country,
+    };
+  },
+  methods: {
+    async getCovid() {
+      let response = await axios.get(`${this.covidUrl}${this.countryCovid}`);
+      this.covidResults = response.data;
+      //   console.log(this.covidResults);
+    },
+  },
+};
+</script>
+
+<style></style>
